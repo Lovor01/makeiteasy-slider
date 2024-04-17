e8ddfbf900ce05531e4060b2ff821cae8a32f291
Lovro Hrust <lovro@makeiteasy.hr>
2024-04-17 18:59:00 +0200

    removed build

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '7044a9d6197dde3d37fd');
--- build/swiper-init.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '60bb71c4a5d9959d39f5');
